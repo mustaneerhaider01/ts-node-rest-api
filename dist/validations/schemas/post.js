78de0b5c3095bcf2ba2726edc8ccc8f5196c552e
Mustaneer Haider <mostanirha1der19@gmail.com>
2024-06-25 19:05:14 +0500

    Delete dist directory

--- dist/validations/schemas/post.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-const postValidationSchemas = {
-    validateCreatePost: Joi.object({
-        title: Joi.string().required().label("Title"),
-        content: Joi.string().required().label("Content"),
-    }),
-};
-export default postValidationSchemas;
